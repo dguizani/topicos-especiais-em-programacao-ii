5442733cf82aecdcb0292c4bcde89a28fed9aec1
Davi GC <63275906+EmployingDGC@users.noreply.github.com>
2022-09-15 17:30:16 -0300

    exercício, código 1006

---
+++ lista-01-iniciante-a/7-1006.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+pA = 2
+pB = 3
+pC = 5
+
+A = float(input()) * pA
+B = float(input()) * pB
+C = float(input()) * pC
+
+media = (A + B + C) / (pA + pB + pC)
+
+print(f"MEDIA = {media:.1f}")
