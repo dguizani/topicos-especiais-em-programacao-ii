6c3a49bfa308c11d45192e2847e08c7c07cec67f
Davi GC <63275906+EmployingDGC@users.noreply.github.com>
2022-09-17 19:27:43 -0300

    exercício, código 1049

---
+++ lista-01-iniciante-b/6-1049.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+p1 = input()
+p2 = input()
+p3 = input()
+
+options = {
+    "vertebrado": {
+        "ave": {
+            "carnivoro": "aguia",
+            "onivoro": "pomba"
+        },
+        "mamifero": {
+            "onivoro": "homem",
+            "herbivoro": "vaca"
+        }
+    },
+    "invertebrado": {
+        "inseto": {
+            "hematofago": "pulga",
+            "herbivoro": "lagarta"
+        },
+        "anelideo": {
+            "hematofago": "sanguessuga",
+            "onivoro": "minhoca"
+        }
+    }
+}
+
+animal = options.get(p1).get(p2).get(p3)
+
+print(animal)
