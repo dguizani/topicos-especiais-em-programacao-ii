eb37dadba123166240caf899fa30a80cd3a5337f
Davi GC <63275906+EmployingDGC@users.noreply.github.com>
2022-09-21 22:42:59 -0300

    exercicio, judge 10038

--- judge/10038.py
+++ judge/10038.py
@@ -1,27 +1,23 @@
 # -*- coding: utf-8 -*-
 
 while True:
-    input_ = [int(x) for x in input().split()]
-
-    if not input_ or input_[0] == 0:
+    try:
+        input_ = [int(x) for x in input().split()]
+        n = input_[0]
+    except:
         break
     
-    n = input_[0]
-
-    values = input_.copy()[1:] + [-1]
-
+    values = input_.copy()[1:] + ["0"]
     compare_values = []
-    qtt_values = n
 
     for i, v in enumerate(values):
-        if values[i + 1] == -1:
+        if values[i + 1] == "0":
             break
         
         abs_value = abs(v - values[i + 1])
         
         compare_values.append(abs_value)
+    
+    is_jolly = all([compare_values.count(x) == 1 for x in range(1, n)])
 
-    if all([compare_values.count(x) == 1 for x in range(1, n)]):
-        print("Jolly")
-    else:
-        print("Not jolly")
+    print("Jolly" if is_jolly else "Not jolly")
